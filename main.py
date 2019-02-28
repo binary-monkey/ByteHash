63d9d2a221a985f09543887b1bf9a067aa657c33
rafaelromon <rafaelromonsagredo@opendeusto.es>
2019-02-28 19:23:12 +0100

    Points function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 
+LIM = 10
+
+
 def parse_input(file):
-    photos = []
+    photos_h = []
+    photos_v = []
 
     with open(file, "r") as f:
 
@@ -26,11 +30,14 @@ def parse_input(file):
 
             photo.append(tags)
 
-            photos.append(photo)
+            if photo[0] == "H":
+                photos_h.append(photo)
+            else:
+                photos_v.append(photo)
 
             cont += 1
 
-    return photos
+    return photos_h, photos_v
 
 
 def is_vertical(photo):
@@ -41,15 +48,36 @@ def tags(photo):
     return photo[2]
 
 
+def sort_photos(photos):
+    return sorted(photos, key=lambda x: len(x[2]))[::-1]
+
+
 def main():
     photos = parse_input("data/a_example.txt")
 
-    for photo in photos:
+    photos_h = photos[0]
+    photos_v = photos[1]
+
+    for photo in photos_h:
         print(photo)
 
     print()
 
-    print(get_points(photos[2], photos[3]))
+    for photo in photos_v:
+        print(photo)
+
+    print()
+
+    print(sort_photos(photos_h))
+
+
+def group_v(photos_v):
+    pairs = []
+    for i in range(len(photos_v)/2):
+        matches = [0] * LIM
+        for j in range(i+1, i+1+LIM):
+            matches[j] = common_tags(photos_v[i], photos_v[j])
+
 
 
 def get_points(photo1, photo2):
