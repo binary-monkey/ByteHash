bdbd7e983c116e74e4468eb138c7c9754ab2be00
rafaelromon <rafaelromonsagredo@opendeusto.es>
2019-02-28 19:46:50 +0100

    Points function

--- main.py
+++ main.py
@@ -52,35 +52,32 @@ def sort_photos(photos):
     return sorted(photos, key=lambda x: len(x[2]))[::-1]
 
 
-def main():
-    photos = parse_input("data/a_example.txt")
-
-    photos_h = photos[0]
-    photos_v = photos[1]
-
-    for photo in photos_h:
-        print(photo)
-
-    print()
-
-    for photo in photos_v:
-        print(photo)
-
-    print()
-
-    print(sort_photos(photos_h))
-
-
 def group_v(photos_v):
+    used = []
     pairs = []
-    for i in range(len(photos_v)/2):
-        matches = [0] * LIM
-        for j in range(i+1, i+1+LIM):
-            matches[j] = common_tags(photos_v[i], photos_v[j])
+
+    for i in range(int(len(photos_v) / 2)):
+        matches = []
+
+        for j in range(0, LIM):
+            try:
+                if (j + i + 1) not in used:
+                    matches.append(common_tags(photos_v[i], photos_v[j + i + 1]))
+            except IndexError:
+                break
+
+        index = matches.index(min(matches))
+        index = i + 1 + index
+
+        pair = [photos_v[i], photos_v[index]]
+
+        pairs.append(pair)
+        used.append(index)
+
+    return pairs
 
 
-
-def get_points(photo1, photo2):
+def get_min_points(photo1, photo2):
     n_common = common_tags(photo1, photo2)
     n_1 = len(tags(photo1)) - n_common
     n_2 = len(tags(photo2)) - n_common
@@ -91,6 +88,26 @@ def get_points(photo1, photo2):
 def common_tags(photo1, photo2):
     return len(set(tags(photo1)).intersection(tags(photo2)))
 
+
+def main():
+    # photos = parse_input("data/a_example.txt")
+    # photos = parse_input("data/c_memorable_moments.txt")
+    photos = parse_input("data/d_pet_pictures.txt")
+
+    photos_h = photos[0]
+    photos_v = photos[1]
+
+    # for photo in photos_h:
+    #     print(photo)
+    #
+    # print()
+    #
+    # for photo in photos_v:
+    #     print(photo)
+    #
+    # print()
+    print(len(photos_v))
+    print(len(group_v(photos_v)))
 
 if __name__ == '__main__':
     main()
